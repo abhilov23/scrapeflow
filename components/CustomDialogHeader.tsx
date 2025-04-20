5d5dd64069bbab54e663536d977c946fa48de6b5
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-20 23:57:37 +0530

    another test added

--- components/CustomDialogHeader.tsx
+++ components/CustomDialogHeader.tsx
@@ -3,6 +3,7 @@
 import { LucideIcon } from "lucide-react";
 import { DialogHeader, DialogTitle } from "./ui/dialog";
 import { cn } from "@/lib/utils";
+import { Separator } from "./ui/separator";
 
 interface Props {
     title?:string;
@@ -17,13 +18,22 @@ interface Props {
 
 export default function CustomDialogHeader(props: Props){
 
-   const Icon = props.icon;
-
     return <DialogHeader className="py-6"> 
          <DialogTitle asChild>
             <div className="flex flex-col items-center gap-2 mb-2">
-                {Icon && <Icon size={30} className={cn("stroke-primary", props.iconClassName)}/>}
+                {props.icon && <props.icon size={30} className={cn("stroke-primary", props.iconClassName)}/>}
+                {props.title && (
+                    <p className={cn("text-xl text-primary", props.iconClassName)}>
+                      {props.title}  
+                    </p>
+                )}
+               {props.subTitle && (
+                    <p className={cn("text-sm text-muted-foreground", props.subtitleClassName)}>
+                      {props.subTitle}  
+                    </p>
+                )}
             </div>
          </DialogTitle>
+         <Separator/>
     </DialogHeader>
 }
