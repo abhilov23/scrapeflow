0b7e1371d43e170fbe0186e0b6e97997f3473e1a
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-26 14:02:08 +0530

    setup the drag-handler and managed the workflow page

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -1,11 +1,20 @@
+import NodeComponent from "@/app/(dashboard)/workflows/_components/nodes/NodeComponent";
 import { Workflow } from "@/lib/generated/prisma";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
+import { TaskType } from "@/types/task";
 import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
 
+const nodeTypes={
+    FlowScrapeNode:NodeComponent,
+}
+
 
 export default function FlowEditor({workflow}: {workflow:Workflow}) {
-    const [nodes, setNodes, onNodeChange]=useNodesState([]);
+    const [nodes, setNodes, onNodeChange]=useNodesState([
+        CreateFlowNode(TaskType.LAUNCH_BROWSER),
+    ]);
     const [edges, setEdges, onEdgeChange] = useEdgesState([]);
     
     return (
@@ -13,6 +22,7 @@ export default function FlowEditor({workflow}: {workflow:Workflow}) {
           <ReactFlow nodes={nodes} edges={edges}
           onEdgesChange={onEdgeChange}
           onNodesChange={onNodeChange}
+          nodeTypes={nodeTypes}
           >
           <Controls position="top-left"/>
           <Background variant={BackgroundVariant.Dots} gap={12} size={1}/>
