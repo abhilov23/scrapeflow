78a294196b664eb121ea343b42b8b82159d45d0a
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-27 12:11:51 +0530

    setup the topbar

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -4,12 +4,14 @@ import { Workflow } from '@/lib/generated/prisma';
 import React from 'react';
 import {ReactFlowProvider} from "@xyflow/react";
 import FlowEditor from './FlowEditor';
+import Topbar from '@/app/(dashboard)/workflows/_components/topbar/Topbar';
 
 
 export default function Editor({workflow}:{workflow:Workflow}) {
     return (
         <ReactFlowProvider>
             <div className='flex flex-col h-full w-full overflow-hidden'>
+                <Topbar title="Workflow Editor" subtitle={workflow.name}/>
              <section className='flex h-full overflow-auto'>
                     <FlowEditor workflow={workflow}/>
              </section>
