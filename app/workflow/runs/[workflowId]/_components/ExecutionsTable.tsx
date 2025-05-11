f472fc6580f602afb37cdb190694442149c3da87
Abhilov23 <abhilovgupta01@gmail.com>
2025-05-12 00:24:09 +0530

    style executionTable.tsx

--- app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
+++ app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
@@ -5,6 +5,9 @@ import { GetWorkflowExecutions } from '@/actions/workflows/GetWorkflowExecutions
 import { useQuery } from '@tanstack/react-query';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { DatesToDurationString } from '@/lib/helper/dates';
+import { Badge } from '@/components/ui/badge';
+import ExecutionStatusIndicator from './ExecutionStatusIndicator';
+import { WorkflowExecutionStatus } from '@/types/workflow';
 
 
 type InitialDataType = Awaited<ReturnType<typeof GetWorkflowExecutions>>;
@@ -35,7 +38,24 @@ function ExecutionsTable({workflowId, initialData}:{
            {query.data.map((execution)=>{ 
             const duration = DatesToDurationString(execution.completedAt, execution.startedAt)
             return (<TableRow key={execution.id}> 
-                <TableCell>{execution.id}</TableCell>
+                <TableCell>
+                    <div className='flex flex-col'>
+                        <span className='font-semibold'>{execution.id}</span>
+                        <div className='text-muted-foreground text-xs pt-1'> 
+                              <span>Triggred via</span>
+                              <Badge variant={'outline'}>{execution.trigger}</Badge>
+                        </div>
+                         </div>
+                </TableCell>
+                <TableCell> 
+                    <div> 
+                        <div className='flex gap-2 items-center'>
+                            <ExecutionStatusIndicator status={execution.status as WorkflowExecutionStatus}/>
+                            <span className='font-semibold capitalize'>{execution.status}</span>
+                        </div>
+                        <div>{duration}</div>
+                    </div>
+                </TableCell>
             </TableRow>)
            })}
         </TableBody>
